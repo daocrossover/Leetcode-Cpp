35879bc1a846e4b9939639a9055afae6f92cba71
daocrossover <475970883@qq.com>
2018-09-17 16:04:29 -0700

    9.17

---
+++ 380. Insert Delete GetRandom O(1)/Solution1.cpp
@@ -0,0 +1,80 @@
+// Description:
+// Design a data structure that supports all following operations in average O(1) time.
+
+// insert(val): Inserts an item val to the set if not already present.
+// remove(val): Removes an item val from the set if present.
+// getRandom: Returns a random element from current set of elements.
+// Each element must have the same probability of being returned.
+
+// Example:
+// Init an empty set.
+// RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+// randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+// randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+// randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+// randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+// randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+// randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+// randomSet.getRandom();
+
+// hash map can't get a random element in O(1) and vector can't remove an element in O(1)
+
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+class RandomizedSet {
+    unordered_map<int, int> hash;
+    vector<int> nums;
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (hash.find(val) != hash.end()) return false;
+        nums.push_back(val);
+        hash[val] = nums.size() - 1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (hash.find(val) == hash.end()) return false;
+        int end = nums.back();
+        hash[end] = hash[val];
+        nums[hash[val]] = end;
+        nums.pop_back();
+        hash.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
