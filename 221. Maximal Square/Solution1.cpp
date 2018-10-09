b10cd3c0b1af7c91ed21ea7f92cf78ee2f1f295c
daocrossover <475970883@qq.com>
2018-10-08 20:39:20 -0700

    10.8

--- 221. Maximal Square/Solution1.cpp
+++ 221. Maximal Square/Solution1.cpp
@@ -1,17 +1,15 @@
-/* Description
-Given a 2D binary matrix filled with 0's and 1's, 
-find the largest square containing only 1's and return its area.
+// Description:
+// Given a 2D binary matrix filled with 0's and 1's, 
+// find the largest square containing only 1's and return its area.
 
-Example:
-Input: 
+// Example:
+// Input: 
+// 1 0 1 0 0
+// 1 0 1 1 1
+// 1 1 1 1 1
+// 1 0 0 1 0
+// Output: 4
 
-1 0 1 0 0
-1 0 1 1 1
-1 1 1 1 1
-1 0 0 1 0
-
-Output: 4
-*/
 
 #include <vector>
 using namespace std;
@@ -22,7 +20,7 @@ public:
         if (matrix.size() == 0) return 0;
         int m = matrix.size(), n = matrix[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        int res = 0;
+        int res = 0; // the max side length
         for (int i = 0; i < n; ++i) {
             dp[0][i] = matrix[0][i] - '0';
             res = max(res, dp[0][i]);
