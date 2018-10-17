07c0d3a6819018c99324e5031eaf3d4fcb6a4f40
daocrossover <475970883@qq.com>
2018-10-17 09:01:13 -0700

    10.17

---
+++ 11. Container With Most Water/Solution1.cpp
@@ -0,0 +1,33 @@
+// Description:
+// Given n non-negative integers a1, a2, ..., an,
+// where each represents a point at coordinate (i, ai).
+// n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+// Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+// Note: You may not slant the container and n is at least 2.
+
+// Example:
+// Input: [1,8,6,2,5,4,8,3,7]
+// Output: 49
+
+// Two pointers Solution:
+// Initially consider the maxLength between lines, move the pointer at the longer line inwards,
+// we won't gain any increase in area, since it is limited by the shorter line.
+// But moving the shorter line's pointer could turn out to be beneficial
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int m = 0;
+        while (left < right) {
+            m = max(m, min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return m;
+    }
+};
---
+++ 9. Palindrome Number/Solution1.cpp
@@ -0,0 +1,46 @@
+// Description:
+// Determine whether an integer is a palindrome.
+// An integer is a palindrome when it reads the same backward as forward.
+
+// Example 1:
+// Input: 121
+// Output: true
+
+// Example 2:
+// Input: -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-.
+// Therefore it is not a palindrome.
+
+// Example 3:
+// Input: 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+// Follow up:
+// Coud you solve it without converting the integer to a string?
+
+// Using queue
+// Time Complexity: O(logn), Space Complexity: O(logn)
+
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        queue<int> q;
+        int tmp = x;
+        while (tmp > 0) {
+            q.push(tmp % 10);
+            tmp = tmp / 10;
+        }
+        int reverse = 0;
+        while (!q.empty()) {
+            reverse = reverse * 10 + q.front();
+            q.pop();
+        }
+        return x == reverse;
+    }
+};
