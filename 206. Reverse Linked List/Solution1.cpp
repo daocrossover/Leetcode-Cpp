5592a7fcbcae9bd0154b1dc36b0fec8648435fb7
daocrossover <475970883@qq.com>
2018-10-26 13:49:08 -0700

    10.26

---
+++ 206. Reverse Linked List/Solution1.cpp
@@ -0,0 +1,35 @@
+// Description:
+// Reverse a singly linked list.
+
+// Example:
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+
+// Follow up:
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+// Iterative Solution:
+// Time complexity: O(n).
+// Space complexity: O(1).
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *pre = nullptr, *cur = head;
+        while (cur) {
+            ListNode *next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+};
---
+++ 206. Reverse Linked List/Solution2.cpp
@@ -0,0 +1,23 @@
+// Recursive Solution:
+// Time complexity: O(n).
+// Space complexity: O(n).
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !(head->next)) {
+            return head;
+        }
+        ListNode* node = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return node;
+    }
+};
